0cf01f1d36e8aedd6abe225b1bfaeae853213c40
Eli Hayun <eli@venus.home>
2019-12-23 13:15:04 +0200

    adding 1-10 payments

--- app/Http/Controllers/secure/paymentController.php
+++ app/Http/Controllers/secure/paymentController.php
@@ -159,7 +159,7 @@ class paymentController extends Controller
 								 <currency>ILS</currency>
 								 <transactionCode>Phone</transactionCode>
 								 <authNumber/>
-								 <numberOfPayments>1-10</numberOfPayments>
+								 <numberOfPayments>10</numberOfPayments>
 								 <firstPayment/>
 								 <periodicalPayment/>
 								 <validation>TxnSetup</validation>
