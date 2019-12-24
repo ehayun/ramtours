8a34fc6f89009f2e6a9998e07482f240c86a34db
Eli Hayun <eli@venus.home>
2019-12-24 14:58:34 +0200

    adding rtl to ckedit

--- public/assets/admin/js/pages/forms/editors.js
+++ public/assets/admin/js/pages/forms/editors.js
@@ -1,8 +1,10 @@
 $(function () {
     //CKEditor
     CKEDITOR.replaceClass='ckeditor1';
-    CKEDITOR.config.contentsLangDirection = 'ltr';
+    CKEDITOR.config.contentsLangDirection = 'rtl';
     CKEDITOR.config.height = 300;
-    CKEDITOR.config.contentsLanguage = 'en';
-  
+    CKEDITOR.config.contentsLanguage = 'he';
+    config.defaultLanguage = 'he';
+    config.language = 'he';
+    config.language_list = [ 'he:Hebrew:rtl' ];
 });
