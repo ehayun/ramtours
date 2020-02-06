92496e760633b5481d49908369b2f4c9ab67f61a
Eli Hayun <eli@venus.home>
2020-02-06 08:48:36 +0200

    fix problem

--- resources/views/frontend/pages/singal_js/package_details_js.blade.php
+++ resources/views/frontend/pages/singal_js/package_details_js.blade.php
@@ -298,13 +298,13 @@
           $(this).removeClass('li_show')
         }
        });
-       if (has_def) {
-         $('.rami_package_cars select').val($(".rami_package_cars select .def_loc:first").val());
-       }
+      //  if (has_def) {
+      //    $('.rami_package_cars select').val($(".rami_package_cars select .def_loc:first").val());
+      //  }
 
-       if (!has_def) {
+      //  if (!has_def) {
          $('.rami_package_cars select').val($(".rami_package_cars select .opt_show:first").val());
-       }
+      //  }
 
        setup_cart();
        //.opt_show:first
